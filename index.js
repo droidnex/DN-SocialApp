8a4056abdee409ac195bd6b530cc3de60ec70e2c
droidnex <droidnex.in@gmai.com>
2022-02-09 02:58:38 +0530

    port and dependancy bugfix1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const PORT = 4000 || process.env.PORT
+const PORT = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
     res.send('Welcome to Droidnex !')
